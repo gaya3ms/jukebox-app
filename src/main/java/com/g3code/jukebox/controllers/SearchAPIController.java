a6e08d04e6e2a96aeaf5e68bb469ff00ff0f0929
Gayathri Muraleedharan <71779613+gaya3ms@users.noreply.github.com>
2022-01-07 12:07:07 -0500

    Update SearchAPIController.java
    Added exception handler

--- src/main/java/com/g3code/jukebox/controllers/SearchAPIController.java
+++ src/main/java/com/g3code/jukebox/controllers/SearchAPIController.java
@@ -1,9 +1,15 @@
 package com.g3code.jukebox.controllers;
 
+import com.g3code.jukebox.exception.ErrorObject;
+import com.g3code.jukebox.exception.SettingIdNotFound;
 import com.g3code.jukebox.models.Jukebox;
 import com.g3code.jukebox.services.JukeboxSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -20,4 +26,12 @@ public class SearchAPIController {
                                                     ,@RequestParam(required = false) Integer limit){
         return jukeboxSearchService.performSearch(settingId, model, offset, limit);
     }
+
+    @ExceptionHandler()
+    public ResponseEntity<ErrorObject> handleException(SettingIdNotFound ex){
+        ErrorObject error = new ErrorObject(HttpStatus.NOT_FOUND.value(), ex.getMessage());
+        return new ResponseEntity<ErrorObject>(error, HttpStatus.NOT_FOUND);
+    }
+
+
 }
